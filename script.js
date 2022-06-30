a7eaf8457d2a5a925b26f1348765c3bfe6c5a509
IgorSousaa <igoralvesnasc2@gmail.com>
2022-06-30 19:48:07 -0300

    Atividade de Inglês

---
+++ script.js
@@ -0,0 +1,40 @@
+    // ===========================Movies Comedy=============================
+
+const Comedy = [
+    {
+        title: "Big People",
+        director: "Dennis Dugan",
+        year: 2010
+    }
+]
+console.log(Comedy);
+   // ============================Movies Action=============================
+const Action = [
+    {
+        title: "Logan",
+        director: "James Mangold",
+        year: 2017
+    }
+]
+console.log(Action);
+
+// ===============================Movies Fantasy=============================
+const Fantasy = [
+    {
+        title: "Spider-Man: No Return Home",
+        director: "Jon Watts",
+        year: 2021
+    }
+]
+
+console.log(Fantasy);
+// ===============================Atividade Extra=============================
+
+let extra = true;
+if(extra){
+    console.log("MY FAVORIT MOVIE IS UNTIL THE LAST MAN");
+}else{
+    console.log("NOT EVEN 5 STARS");
+}
+
+// ===========================================================================
